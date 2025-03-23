1666de9613ae06281dca30b004a0925851c7a42d
ZrinkaMihaljevic <zmihaljev@tvz.hr>
2025-03-23 20:22:05 +0100

    Create da_vincijev_kod.php

---
+++ da_vincijev_kod.php
@@ -0,0 +1,19 @@
+<?php
+    echo "<h1>Da Vincijev kod</h1>";
+    echo "<p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>";
+    echo '<a href="https://hr.wikipedia.org/Da_Vincijev_kod" target="_blank">Više o knjizi</a>'
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Stranica o Da Vincijevom kodu">
+    <meta name="keywords" content="Da Vinci, Da Vincijev kod, Dan Brown, knjiga">
+    <title>Da Vincijev kod</title>
+</head>
+<body>
+    <!-- Naziv datoteke: da_vincijev_kod.php -->
+</body>
+</html>
